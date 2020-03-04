61251da980c5b8d5d7317c845cb68dd95fe45b9c
Rocío Jamanca <rozzy.1997@gmail.com>
2020-03-04 15:23:04 +0100

    Minor changes - New functions : Get message and Post message

--- controllers/testScripts.js
+++ controllers/testScripts.js
@@ -1,5 +1,14 @@
+
 'use strict';
 
 exports.test = async function (req, res){
     res.status(200).send('It works');
+};
+
+exports.getmessage = async function (req, res){
+    res.status(200).send(req.body);
+};
+
+exports.postmessage = async function (req, res){
+    res.status(200).send(req.body);
 };
--- routes/test.js
+++ routes/test.js
@@ -4,6 +4,10 @@ let router = express.Router();
 let testScripts = require('../controllers/testScripts');
 
 router.get('/get', testScripts.test);
+router.get('/getmsg',testScripts.getmessage);
+
 router.post('/post', testScripts.test);
+router.post('/postmsg',testScripts.postmessage);
+
 
 module.exports = router;
