b432c0b029c25de280fa840de2b75a2519306f98
Rocío Jamanca <rozzy.1997@gmail.com>
2020-04-12 17:13:47 +0200

    Get public key of server at aggregator

--- controllers/aggregatorScripts.js
+++ controllers/aggregatorScripts.js
@@ -1,7 +1,19 @@
 'use strict';
-
+let paillierBigInt = require('paillier-bigint');
 let bigConv = require('bigint-conversion');
 const got = require('got');
+
+let publicKey;
+getKey();
+
+async function getKey(){
+    let response = (await got('http://localhost:3000/calculator/getKey'));
+    response = JSON.parse(response.body);
+    let n = response.n;
+    let g = response.g;
+    publicKey = new paillierBigInt.PublicKey(bigConv.hexToBigint(n),bigConv.hexToBigint(g));
+}
+
 
 exports.sum = async function(req, res) {
     let n = bigConv.hexToBigint(req.body.n);
@@ -22,3 +34,6 @@ exports.multiply = async function(req, res) {
     let response = await got.post('http://localhost:3000/calculator/decrypt', {json: {c: result}, responseType: 'json'});
     res.status(200).send({result: response.body.result});
 }
+
+
+
