0c282ae50e19138d4ef011f5112fdc262ad6f97b
sergi <sergi@unievegroup.com>
2020-05-17 00:02:51 +0200

    removed unused mongo code

--- app.js
+++ app.js
@@ -46,35 +46,3 @@ module.exports = agg;
 let io = socketIO(server);
 let socket = require('./sockets/socketScripts')
 socket.socket(io);
-
-//Mongo database connection
-// mongoose.connect("mongodb://localhost:27017/erasmus",{
-//     useNewUrlParser: true,
-//     reconnectTries : Number.MAX_VALUE,
-//     autoReconnect : true,
-//     useUnifiedTopology: true,
-//     reconnectInterval: 500
-// }).then(() =>{
-//     console.log('Connection to the database successful');
-// }).catch(err =>{
-//     console.log(`Database error: ${err.message}`);
-// });
-//
-// //Handle database connection events
-// mongoose.connection.on('reconnected', () => {
-//     console.log('Database reconnected');
-// });
-// mongoose.connection.on('error', (err) => {
-//     console.log(`Database error: ${err.message}`);
-// });
-// mongoose.connection.on('disconnected', () => {
-//     console.log('Database disconnected');
-//     //If database is disconnected it wil try again
-//     mongoose.connect("mongodb://localhost:27017/erasmus",{
-//         useNewUrlParser: true,
-//         reconnectTries : Number.MAX_VALUE,
-//         autoReconnect : true,
-//         useUnifiedTopology: true,
-//         reconnectInterval: 500
-//     });
-// });
